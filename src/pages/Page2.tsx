fea4f565f0e3b821bac550e91f1a3d5c79394770
soo21 <shyoun21@naver.com>
2024-10-15 21:14:09 +0900

    메뉴콘솔로그

--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -1,9 +1,26 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Page2 = ()=>{
     return (
     <div>
         <h2>Im page2</h2>
     </div>
     )
+}
+
+const Menus = () => {
+    
+    const menuClick = () => {
+        axios.get('http://192.168.0.93:8080/test123').then(response => {
+            const menus = response.data;
+            console.log(menus);
+      });
+      return (
+        <div>
+            <button onClick={menuClick}>메뉴 표시</button>
+        </div>
+    )};
 }
 
 export default Page2;
