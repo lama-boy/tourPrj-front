61da96b90279e70c7751afa1071e9524d22f9b69
ksw96 <ghkfzha1@gmail.com>
2024-12-16 19:44:15 +0900

    add page4

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './styles/main.scss'
 import Login from './pages/Login';
 import Page2 from './pages/Page2';
 import Page3 from './pages/Page3';
-
+import Page4 from './pages/Page4';
 
 function App() {
   /*
@@ -26,12 +26,14 @@ function App() {
             <Link className='navLinks' to="/login">로그인</Link>
             <Link className='navLinks' to="/page1">프로젝트</Link>
             <Link className='navLinks' to="/page2">학원</Link>
+            <Link className='navLinks' to="/page3">테스트 페이지</Link>
           </div>
         </nav>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/page1" element={<Page2 />} />
           <Route path="/page2" element={<Page3 />} />
+          <Route path="/page3" element={<Page4 />} />
         </Routes>
       </div>
     </BrowserRouter>
